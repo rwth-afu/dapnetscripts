47d0eed861b7590d411bac3ab6131a2bfdb59d98
dj7lc <dj7lc@t-online.de>
2024-10-28 23:32:59 +0100

    Linter formatting

--- mowas/mowas.py
+++ mowas/mowas.py
@@ -7,51 +7,56 @@ Usage::
 origin https://gist.github.com/mdonkers/63e115cc0c79b4f6b8b3a6b797e485c7
 """
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("GET request for {}".format(self.path).encode("utf-8"))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        content_length = int(self.headers["Content-Length"])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
         logging.info(
             "POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-            str(self.path), str(self.headers), post_data.decode('utf-8'),
+            str(self.path),
+            str(self.headers),
+            post_data.decode("utf-8"),
         )
 
         self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("POST request for {}".format(self.path).encode("utf-8"))
 
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(
-        handlers=[logging.FileHandler(filename="mowas.log", encoding="utf-8", mode="a+"), logging.StreamHandler(),],
+        handlers=[
+            logging.FileHandler(filename="mowas.log", encoding="utf-8", mode="a+"),
+            logging.StreamHandler(),
+        ],
         format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
         level=logging.DEBUG,
     )
-    server_address = ('', port)
+    server_address = ("", port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+    logging.info("Starting httpd...\n")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    logging.info('Stopping httpd...\n')
+    logging.info("Stopping httpd...\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 2:
